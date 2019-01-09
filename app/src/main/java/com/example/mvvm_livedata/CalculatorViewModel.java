d032e973de8876e64f297070a62269b0fbd91ea9
Sidd <user@example.com>
2019-01-09 17:56:40 +0000

    First Commit

---
+++ app/src/main/java/com/example/mvvm_livedata/CalculatorViewModel.java
@@ -0,0 +1,34 @@
+package com.example.mvvm_livedata;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+
+public class CalculatorViewModel {
+    private int result = 0;
+    private int numberOne = 0;
+    private int numberTwo = 0;
+
+    private MutableLiveData<String> results = new MutableLiveData<>();
+
+    public LiveData<String> getResult() {
+        return results;
+    }
+
+    public void add(String a, String b) {
+        getNumbersFromInput(a, b);
+
+        result = numberOne + numberTwo;
+
+        results.setValue(String.valueOf(result));
+    }
+
+    private void getNumbersFromInput(String a, String b) {
+        try {
+            numberOne = Integer.valueOf(a);
+            numberTwo = Integer.valueOf(b);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mvvm_livedata/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.mvvm_livedata
+
+import android.arch.lifecycle.Observer
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val resultObserver = Observer<String> {
+            tvResult.text = it
+        }
+
+        val viewModel = CalculatorViewModel()
+        viewModel.result.observe(this, resultObserver)
+
+        btnAdd.setOnClickListener {
+            viewModel.add(etNumberOne.text.toString(),
+                etNumberTwo.text.toString())
+        }
+    }
+}
